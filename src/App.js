155efff2fc85950856d02872f27c1cee7b572a28
My Name <haidang11082001@email.com>
2022-01-21 15:35:33 +0700

    toi uu code

--- src/App.js
+++ src/App.js
@@ -19,108 +19,68 @@ function App() {
       }
       return array
   }
-  const [positions0,setPositions0] = useState([]);
-  const [positions1,setPositions1] = useState([]);
-  const [positions2,setPositions2] = useState([]);
-  const [positions3,setPositions3] = useState([]);
-  const [positions4,setPositions4] = useState([]);
-  const [positions5,setPositions5] = useState([]);
-  const [positions6,setPositions6] = useState([]);
-  const [positions7,setPositions7] = useState([]);
+	 	const [positions,setPositions] = useState([]);
+//   const [positions1,setPositions1] = useState([]);
+//   const [positions2,setPositions2] = useState([]);
+//   const [positions3,setPositions3] = useState([]);
+//   const [positions4,setPositions4] = useState([]);
+//   const [positions5,setPositions5] = useState([]);
+//   const [positions6,setPositions6] = useState([]);
+//   const [positions7,setPositions7] = useState([]);
+
   useEffect(() => {
     fetch("https://s3.amazonaws.com/CMSTest/squaw_creek_container_info.xml?fbclid=IwAR0YUCBa-S_HrMLiXeJTsXdmBXJbLa3PoyCBjJKNlRggthLfNYcCxTogiuo")
         .then(res => res.text())
         .then(data => {
             var xml = new XMLParser().parseFromString(data); 
-            setPositions0(Cartesian3.fromDegreesArrayHeights(createPolygon(0,8,xml)))
-            setPositions1(Cartesian3.fromDegreesArrayHeights(createPolygon(7,14,xml)))
-            setPositions2(Cartesian3.fromDegreesArrayHeights(createPolygon(14,18,xml)))
-            setPositions3(Cartesian3.fromDegreesArrayHeights(createPolygon(17,22,xml)))
-            setPositions4(Cartesian3.fromDegreesArrayHeights(createPolygon(22,25,xml)))
-            setPositions5(Cartesian3.fromDegreesArrayHeights(createPolygon(25,28,xml)))
-            setPositions6(Cartesian3.fromDegreesArrayHeights(createPolygon(28,31,xml)))
-            setPositions7(Cartesian3.fromDegreesArrayHeights(createPolygon(31,34,xml)))
+            var polygon0 = Cartesian3.fromDegreesArrayHeights(createPolygon(0,8,xml))
+            var polygon1 = (Cartesian3.fromDegreesArrayHeights(createPolygon(7,14,xml)))
+            var polygon2 = (Cartesian3.fromDegreesArrayHeights(createPolygon(14,18,xml)))
+            var polygon3 = (Cartesian3.fromDegreesArrayHeights(createPolygon(17,22,xml)))
+            var polygon4 = (Cartesian3.fromDegreesArrayHeights(createPolygon(22,25,xml)))
+            var polygon5 = (Cartesian3.fromDegreesArrayHeights(createPolygon(25,28,xml)))
+            var polygon6 = (Cartesian3.fromDegreesArrayHeights(createPolygon(28,31,xml)))
+            var polygon7 = (Cartesian3.fromDegreesArrayHeights(createPolygon(31,34,xml)))
+			setPositions(pre => [...pre,polygon0])
+			setPositions(pre => [...pre,polygon1])
+			setPositions(pre => [...pre,polygon2])
+			setPositions(pre => [...pre,polygon3])
+			setPositions(pre => [...pre,polygon4])
+			setPositions(pre => [...pre,polygon5])
+			setPositions(pre => [...pre,polygon6])
+			setPositions(pre => [...pre,polygon7])
+			
+			
         })
         .catch(err => console.log(err));
 },[])
+	const renderPolygon = () =>{
+		let xhml = null
+			xhml = (	
+				positions.map((pos , index) =>{	
+					return (
+						<Entity key={index}>
+							<PolygonGraphics
+								hierarchy={pos}
+								perPositionHeight={true}
+								material={Color.GREEN.withAlpha(0.5)}
+								outline = {true}
+								outlineColor = {Color.BLACK}
+							/>
+						</Entity>
+						)
+					})
+				)
+		return xhml
+	}
   return (
     <div>
       <Viewer full>
-        <Entity>
-          <PolygonGraphics
-              hierarchy={positions0}
-              perPositionHeight={true}
-              material={Color.GREEN.withAlpha(0.5)}
-              outline = {true}
-              outlineColor = {Color.BLACK}
-            />
-        </Entity>
-        <Entity>
-          <PolygonGraphics
-              hierarchy={positions1}
-              perPositionHeight={true}
-              material={Color.GREEN.withAlpha(0.5)}
-              outline = {true}
-              outlineColor = {Color.BLACK}
-            />
-        </Entity>
-        <Entity>
-          <PolygonGraphics
-              hierarchy={positions2}
-              perPositionHeight={true}
-              material={Color.GREEN.withAlpha(0.5)}
-              outline = {true}
-              outlineColor = {Color.BLACK}
-            />
-        </Entity>
-        <Entity>
-          <PolygonGraphics
-              hierarchy={positions3}
-              perPositionHeight={true}
-              material={Color.GREEN.withAlpha(0.5)}
-              outline = {true}
-              outlineColor = {Color.BLACK}
-            />
-        </Entity>
-        <Entity>
-          <PolygonGraphics
-              hierarchy={positions4}
-              perPositionHeight={true}
-              material={Color.GREEN.withAlpha(0.5)}
-              outline = {true}
-              outlineColor = {Color.BLACK}
-            />
-        </Entity>
-        <Entity>
-          <PolygonGraphics
-              hierarchy={positions5}
-              perPositionHeight={true}
-              material={Color.GREEN.withAlpha(0.5)}
-              outline = {true}
-              outlineColor = {Color.BLACK}
-            />
-        </Entity>
-        <Entity>
-          <PolygonGraphics
-              hierarchy={positions6}
-              perPositionHeight={true}
-              material={Color.GREEN.withAlpha(0.5)}
-              outline = {true}
-              outlineColor = {Color.BLACK}
-            />
-        </Entity>
-        <Entity>
-          <PolygonGraphics
-              hierarchy={positions7}
-              perPositionHeight={true}
-              material={Color.GREEN.withAlpha(0.5)}
-              outline = {true}
-              outlineColor = {Color.BLACK}
-            />
-        </Entity>
+		  
+        {renderPolygon()}
         <CameraFlyTo  duration={8} 
-                  destination={Cartesian3.fromDegrees(-93.62033081054688, 42.01864242553711, 278.75982666015625)}     
-                  />
+            destination={Cartesian3.fromDegrees(-93.62033081054688, 42.01864242553711, 278.75982666015625)}     
+        />
       </Viewer>
     </div>
     
